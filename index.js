b32b52da3a9928c0342fd5e06c673c0faf47829c
Unilago <Unilago@example.com>
2026-03-17 20:20:18 -0300

    1.2

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
-const express = require("express")
-const { engine } = require("express-handlebars")
-const bodyParser = require('body-parser')
-const path = require("path")
-const routes = require("./routes")
+const express = require('express')
+const { engine } = require('express-handlebars')
 
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-
-app.engine('handlebars', engine({ defaultLayout: 'main' }))
+app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
-app.use(express.static(path.join(__dirname, 'views/css/')))
-app.use(express.static(path.join(__dirname, 'views/js/')))
+const routes = require('./routes')
+app.use('/', routes)
 
-app.use("/", routes)
-
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
-})
+app.listen(3000)
--- routes.js
+++ routes.js
@@ -1,15 +1,17 @@
-const express = require("express")
+const express = require('express')
 const router = express.Router()
 
-router.get("/", (req, res) => {
-    res.render('index')
+router.get('/', (req, res) => {
+  res.render('index') // sua pagina1
 })
 
-router.get("/sobre/{:id}", (req, res) => {
+router.get('/pagina2', (req, res) => {
+  res.render('index2') // sua pagina2
+})
 
-    const id = req.params.id
-    
-    res.render("sobre", { id })
+router.get('/sobre/:id', (req, res) => {
+  const id = req.params.id
+  res.render('sobre', { id })
 })
 
 module.exports = router
