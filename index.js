4d10287f4640cf24ce45af246450797cd9f58313
Unilago <Unilago@example.com>
2026-03-17 21:39:02 -0300

    1.3

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
-const { engine } = require('express-handlebars')
+const exphbs = require('express-handlebars')
+const routes = require('./routes')
 
 const app = express()
 
-app.engine('handlebars', engine())
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+app.set('views', './views')
 
-const routes = require('./routes')
+app.use(express.static('public'))
+
 app.use('/', routes)
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Servidor rodando em http://localhost:3000')
+})
--- routes.js
+++ routes.js
@@ -2,16 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.render('index') // sua pagina1
+  res.render('index', { title: 'Página 1' })
 })
 
 router.get('/pagina2', (req, res) => {
-  res.render('index2') // sua pagina2
-})
-
-router.get('/sobre/:id', (req, res) => {
-  const id = req.params.id
-  res.render('sobre', { id })
+  res.render('index2', { title: 'Página 2' })
 })
 
 module.exports = router
