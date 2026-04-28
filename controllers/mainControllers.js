87ed0eea7a91df0773b123050d2c95f65c272134
labuunilago <labu@unilago.edu>
2026-04-28 19:38:49 -0300

    2.0

---
+++ controllers/mainControllers.js
@@ -0,0 +1,16 @@
+exports.main = (req, res) => {
+    res.render('index')
+}
+
+exports.sobre = (req, res) => {
+    
+    res.render("sobre")
+}
+
+exports.sobrePost = (req, res) => { 
+    console.log(req.body)
+    const nome = req.body.user_name
+    
+    res.render("profile", { nome })
+
+}
